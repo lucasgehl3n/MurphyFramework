c75242485aadeb468209ad6012c0441e813020b8
ViniMeng <vinimeng@hotmail.com>
2019-06-21 23:36:58 -0300

    jexcel

--- application/views/header.php
+++ application/views/header.php
@@ -22,6 +22,8 @@
         <link href="<?=base_url();?>assets/css/pickadate/classic.css" rel="stylesheet">
         <link href="<?=base_url();?>assets/css/pickadate/classic.date.css" rel="stylesheet">
         <link href="<?=base_url();?>assets/css/pickadate/classic.time.css" rel="stylesheet">
+        <link href="<?=base_url();?>assets/css/jexcel/japp.css" rel="stylesheet">
+        <link href="<?=base_url();?>assets/css/jexcel/jexcel.css" rel="stylesheet">
         <link href="<?=base_url();?>assets/fontawesome/css/font-awesome.min.css" rel="stylesheet">
         <!-- Javascript -->
         <script src="<?=base_url();?>assets/js/jquery.js"></script>
@@ -34,6 +36,8 @@
         <script src="<?=base_url();?>assets/js/pickadate/picker.js"></script>
         <script src="<?=base_url();?>assets/js/pickadate/picker.date.js"></script>
         <script src="<?=base_url();?>assets/js/pickadate/picker.time.js"></script>
+        <script src="<?=base_url();?>assets/js/jexcel/japp.js"></script>
+        <script src="<?=base_url();?>assets/js/jexcel/jexcel.js"></script>
     </head>
     <!-- Header -->
     <header class="w3-top">
